ac7afbbd662fe8b2d813285da97ab70dd38df4f9
matti <mattikjellstadli@icloud.com>
2023-08-24 23:32:22 +0200

    Removed max window size

--- src/main/java/com/calmat/keyaqua/ui/KeyAquaApplication.java
+++ src/main/java/com/calmat/keyaqua/ui/KeyAquaApplication.java
@@ -2,29 +2,43 @@ package com.calmat.keyaqua.ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 
-public class HelloApplication extends Application {
+public class KeyAquaApplication extends Application {
+
+    private static Stage stg;
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/calmat/keyaqua/lockedKeyMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(KeyAquaApplication.class.getResource("/com/calmat/keyaqua/lockedKeyMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 650);
+        stg = stage;
         stage.setMinHeight(650);
         stage.setMinWidth(700);
-        stage.setMaxWidth(1200);
-        stage.setMaxHeight(700);
         stage.setTitle("KeyAqua");
         stage.setScene(scene);
         scene.getStylesheets().add(Objects.requireNonNull(this.getClass().getResource("/com/calmat/keyaqua/themes/theme.css")).toExternalForm());
         stage.getIcons().add(new Image("/com/calmat/keyaqua/images/6676583.png"));
         stage.show();
     }
+
+    /**
+     * This method changes the fxml file in the same scene.
+     * @param fxml name of the requested fxml file.
+     * @throws IOException Exception
+     */
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/calmat/keyaqua/"+ fxml)));
+        stg.getScene().setRoot(pane);
+    }
+
 
     public static void main(String[] args) {
         launch();
