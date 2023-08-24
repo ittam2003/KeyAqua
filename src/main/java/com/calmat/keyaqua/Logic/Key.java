f5be29f677434a2e70a54a528d6cfc43b89fc56d
matti <mattikjellstadli@icloud.com>
2023-08-24 23:31:44 +0200

    New class key

---
+++ src/main/java/com/calmat/keyaqua/Logic/Key.java
@@ -0,0 +1,79 @@
+package com.calmat.keyaqua.Logic;
+
+/**
+ * Represents a key... l8r
+ */
+
+public class Key {
+
+    private String name;
+    private String key;
+
+    public Key(String name, String key){
+        this.name = name;
+        this.key = key;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getKey(){
+        return this.key;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setKey(String key){
+        this.key = name;
+    }
+
+
+    public static class KeyBuilder {
+        private String name;
+        private String key;
+
+        /**
+         * Creates a new instance of the KeyBuilder class.
+         */
+        public KeyBuilder() {
+            this.name = "";
+            this.key = "";
+        }
+
+        /**
+         * Sets the name of the player.
+         *
+         * @param name Player name.
+         * @return The player name.
+         */
+        public KeyBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
+         * Sets the image of the player.
+         *
+         * @param key Player image.
+         * @return The player image.
+         */
+        public KeyBuilder key(String key) {
+            this.key = key;
+            return this;
+        }
+
+
+        /**
+         * Builds the player.
+         *
+         * @return The player.
+         */
+        public Key build() {
+            return new Key(name, key);
+        }
+
+    }
+}
