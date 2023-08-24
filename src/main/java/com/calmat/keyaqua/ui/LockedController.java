fb91e0b22ce31f456c6e0dff2c9a9de0991f9402
matti <mattikjellstadli@icloud.com>
2023-08-24 23:33:41 +0200

    Added multiple user functionality

--- src/main/java/com/calmat/keyaqua/ui/LockedController.java
+++ src/main/java/com/calmat/keyaqua/ui/LockedController.java
@@ -1,25 +1,23 @@
 package com.calmat.keyaqua.ui;
 
+import com.calmat.keyaqua.Logic.Database;
+import com.calmat.keyaqua.Logic.User;
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 import java.io.IOException;
-import java.util.Objects;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LockedController {
+public class LockedController implements Initializable {
 
 
 
@@ -27,39 +25,56 @@ public class LockedController {
     private PasswordField passwordField;
 
     @FXML
-    private Button passwordButton;
+    private TextField usernameField;
 
     @FXML
     private Label errorMessage;
 
     public void password(){
-        String password;
-        File file5 = new File("Keys/password.dat");
-        try (BufferedReader reader = new BufferedReader(new FileReader(file5))) {
-            password = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        String keyText = passwordField.getText();
-        if (keyText.equals(password) | password == null){
-            unlock();
-        } else {
-            errorMessage.setText("Wrong password...");
+        if (!usernameField.getText().isEmpty() && !passwordField.getText().isEmpty()){
+            User user = new User(usernameField.getText(), passwordField.getText());
+            Database database = new Database();
+            if (database.checkUser(user)){
+                try {
+                    database.setActiveUser(user.getUsername());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                unlock();
+            }
+        }else {
+            errorMessage.setText("Wrong username/password...");
         }
     }
 
     public void unlock() {
+        KeyAquaApplication m = new KeyAquaApplication();
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/calmat/keyaqua/KeyMenu.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root,1000, 650);
-            scene.getStylesheets().add(Objects.requireNonNull(this.getClass().getResource("/com/calmat/keyaqua/themes/theme.css")).toExternalForm());
-            Stage stage = (Stage) passwordButton.getScene().getWindow();
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
+            m.changeScene("keyMenu.fxml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    @FXML
+    private ImageView secureIcon;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(secureIcon);
+        translate.setDuration(Duration.millis(3000));
+        translate.setCycleCount(Animation.INDEFINITE);
+        translate.setByY(-15);
+        translate.setAutoReverse(true);
+        translate.play();
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(secureIcon);
+        fade.setDuration(Duration.millis(2000));
+        fade.setInterpolator(Interpolator.LINEAR);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.play();
+
     }
 }
