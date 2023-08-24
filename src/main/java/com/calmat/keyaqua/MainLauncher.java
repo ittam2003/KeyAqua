ed17e58d252603adc545e5b9b464a92470fb7b21
matti <mattikjellstadli@icloud.com>
2023-08-24 23:33:09 +0200

    Renamed

--- src/main/java/com/calmat/keyaqua/MainLauncher.java
+++ src/main/java/com/calmat/keyaqua/MainLauncher.java
@@ -1,10 +1,10 @@
 package com.calmat.keyaqua;
 
 
-import com.calmat.keyaqua.ui.HelloApplication;
+import com.calmat.keyaqua.ui.KeyAquaApplication;
 
 public class MainLauncher {
     public static void main(String[] args) {
-        HelloApplication.main(args);
+        KeyAquaApplication.main(args);
     }
 }
